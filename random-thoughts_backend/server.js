1853146b9f500dbfb5d22796e40ab54b93cba870
rohit-workspace069 <rohitwork069@gmail.com>
2024-07-13 19:38:22 +0530

    added git ignore

--- random-thoughts_backend/server.js
+++ random-thoughts_backend/server.js
@@ -1,1 +1,10 @@
-import express from'express';
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT;
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+} );
